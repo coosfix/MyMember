e490c06241fa3e9f9441b961665bcdc33245c5fa
stepmania003 <stepmania003@gmail.com>
2019-04-29 21:11:53 +0800

    第二次

--- Member/Member/Form1.Designer.cs
+++ Member/Member/Form1.Designer.cs
@@ -28,13 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Pwordtextbox = new System.Windows.Forms.TextBox();
+            this.UsertextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "密碼";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "帳號";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "註冊";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "登入";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Pwordtextbox
+            // 
+            this.Pwordtextbox.Location = new System.Drawing.Point(64, 44);
+            this.Pwordtextbox.Name = "Pwordtextbox";
+            this.Pwordtextbox.Size = new System.Drawing.Size(120, 22);
+            this.Pwordtextbox.TabIndex = 8;
+            // 
+            // UsertextBox
+            // 
+            this.UsertextBox.Location = new System.Drawing.Point(64, 16);
+            this.UsertextBox.Name = "UsertextBox";
+            this.UsertextBox.Size = new System.Drawing.Size(120, 22);
+            this.UsertextBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(313, 126);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Pwordtextbox);
+            this.Controls.Add(this.UsertextBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Pwordtextbox;
+        private System.Windows.Forms.TextBox UsertextBox;
     }
 }
 
